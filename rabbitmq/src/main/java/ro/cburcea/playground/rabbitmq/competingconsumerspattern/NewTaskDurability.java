c21a482b83b0f0f3efac31b5c96064386402495f
catalin-burcea <burcea.iulian.catalin@gmail.com>
2020-01-30 11:03:11 +0200

    added a competing-consumers-pattern example in rabbitmq module

---
+++ rabbitmq/src/main/java/ro/cburcea/playground/rabbitmq/competingconsumerspattern/NewTaskDurability.java
@@ -0,0 +1,27 @@
+package ro.cburcea.playground.rabbitmq.competingconsumerspattern;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+public class NewTaskDurability {
+    private final static String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection()) {
+            Channel channel = connection.createChannel();
+
+            boolean durable = true;
+            channel.queueDeclare(TASK_QUEUE_NAME, durable, false, false, null);
+            String message = "a b. c d. e f";
+            channel.basicPublish("",
+                    TASK_QUEUE_NAME,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+} 
---
+++ rabbitmq/src/main/java/ro/cburcea/playground/rabbitmq/competingconsumerspattern/WorkerFairDispatch1.java
@@ -0,0 +1,46 @@
+package ro.cburcea.playground.rabbitmq.competingconsumerspattern;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class WorkerFairDispatch1 {
+
+    private final static String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        boolean durable = true;
+        channel.queueDeclare(TASK_QUEUE_NAME, durable, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        channel.basicQos(1); // accept only one unack-ed message at a time (see below)
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+
+            try {
+                doWork(message);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                System.out.println(" [x] Done");
+            }
+        };
+        boolean autoAck = false;
+        channel.basicConsume(TASK_QUEUE_NAME, autoAck, deliverCallback, consumerTag -> { });
+    }
+
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch: task.toCharArray()) {
+            if (ch == '.') Thread.sleep(3000);
+        }
+    }
+}
