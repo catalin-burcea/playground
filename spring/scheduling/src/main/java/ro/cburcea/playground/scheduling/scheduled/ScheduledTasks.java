36d66858e06f9bd7380480ff4fe3bc46ea0409b1
catalin-burcea <burcea.iulian.catalin@gmail.com>
2020-03-26 18:03:21 +0200

    added scheduling examples using Spring's ThreadPoolTaskScheduler class

--- spring/scheduling/src/main/java/ro/cburcea/playground/scheduling/scheduled/ScheduledTasks.java
+++ spring/scheduling/src/main/java/ro/cburcea/playground/scheduling/scheduled/ScheduledTasks.java
@@ -1,4 +1,4 @@
-package ro.cburcea.playground.scheduling;
+package ro.cburcea.playground.scheduling.scheduled;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
---
+++ spring/scheduling/src/main/java/ro/cburcea/playground/scheduling/taskscheduler/TaskSchedulerApplication.java
@@ -0,0 +1,12 @@
+package ro.cburcea.playground.scheduling.taskscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskSchedulerApplication.class);
+    }
+}
