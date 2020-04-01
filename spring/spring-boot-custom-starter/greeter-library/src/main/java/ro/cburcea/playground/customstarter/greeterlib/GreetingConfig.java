7f61238f18fb643acd021fb2d91b144f5d94729b
catalin-burcea <burcea.iulian.catalin@gmail.com>
2020-04-01 13:42:38 +0300

    created a spring boot custom starter

---
+++ spring/spring-boot-custom-starter/greeter-library/src/main/java/ro/cburcea/playground/customstarter/greeterlib/GreetingConfig.java
@@ -0,0 +1,9 @@
+package ro.cburcea.playground.customstarter.greeterlib;
+
+import java.util.Properties;
+
+public class GreetingConfig extends Properties {
+
+    private static final long serialVersionUID = 5662570853707247891L;
+
+}
