533ac7e77149ac2f2d565b4e6b6fcddb25c7f04f
catalin-burcea <burcea.iulian.catalin@gmail.com>
2020-04-05 22:07:50 +0300

    created a spring security oauth simple app: resource and authorization servers

---
+++ spring-security/oauth2-sample-app/oauth2-authorization-server/src/main/java/ro/cburcea/playground/springsecurity/oauth/authserver/OAuth2AuthServerApp.java
@@ -0,0 +1,48 @@
+package ro.cburcea.playground.springsecurity.oauth.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * http://localhost:8081/oauth/authorize
+ * http://localhost:8081/oauth/token
+ *
+ * PASSWORD_CREDENTIALS grant type
+ * 		using CURL:
+ * 			[POST] curl reader:secret@localhost:8081/oauth/token -d grant_type=password -d username=user -d password=password
+ * 			[POST] curl writer:secret@localhost:8081/oauth/token -d grant_type=password -d username=user -d password=password
+ * 			[POST] curl noscopes:secret@localhost:8081/oauth/token -d grant_type=password -d username=user -d password=password
+ * 		using Postman:
+ 			[POST] RequestHeaders: Authorization: Basic c2NyaXB0OnNlY3JldA==
+ * 			RequestBody:
+ * 				grant_type: "client_credentials"
+ * 				scope: "message_read"
+ * 				username: "user"
+ * 				password: "password"
+ *
+ * 	CLIENT_CREDENTIALS grant type
+ * 		using CURL:
+ * 			[POST] curl script:secret@localhost:8081/oauth/token -d grant_type=client_credentials
+ * 		using Postman:
+ * 			[POST] RequestHeaders: Authorization: Basic c2NyaXB0OnNlY3JldA==
+ * 			RequestBody:
+ * 				grant_type: "authorization_code"
+ * 				scope: "message_read"
+ *
+ * 	AUTHORIZATION_CODE grant type
+ 		using Postman:
+ * 			[POST] RequestHeaders: Authorization: Basic c2NyaXB0OnNlY3JldA==
+ * 			RequestBody:
+ * 				grant_type: "authorization_code"
+ * 				code: "ZgeZbL"
+ * 				redirect_uri: "http://localhost:8080/login"
+ * 				client_id: "web"
+ *
+ */
+@SpringBootApplication
+public class OAuth2AuthServerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2AuthServerApp.class, args);
+	}
+}
---
+++ spring-security/oauth2-sample-app/oauth2-authorization-server/src/main/java/ro/cburcea/playground/springsecurity/oauth/authserver/config/AuthorizationServerConfiguration.java
@@ -0,0 +1,136 @@
+package ro.cburcea.playground.springsecurity.oauth.authserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.security.KeyPair;
+
+/**
+ * An instance of Legacy Authorization Server (spring-security-oauth2) that uses a single,
+ * not-rotating key and exposes a JWK endpoint.
+ * <p>
+ * See
+ * <a
+ * target="_blank"
+ * href="https://docs.spring.io/spring-security-oauth2-boot/docs/current-SNAPSHOT/reference/htmlsingle/">
+ * Spring Security OAuth Autoconfig's documentation</a> for additional detail
+ *
+ * @since 5.1
+ */
+@EnableAuthorizationServer
+@Configuration
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+    private AuthenticationManager authenticationManager;
+    private KeyPair keyPair;
+    private boolean jwtEnabled;
+
+    public AuthorizationServerConfiguration(
+            AuthenticationConfiguration authenticationConfiguration,
+            KeyPair keyPair,
+            @Value("${security.oauth2.authorizationserver.jwt.enabled:true}") boolean jwtEnabled) throws Exception {
+
+        this.authenticationManager = authenticationConfiguration.getAuthenticationManager();
+        this.keyPair = keyPair;
+        this.jwtEnabled = jwtEnabled;
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        // @formatter:off
+        clients.inMemory()
+                .withClient("reader")
+                    .authorizedGrantTypes("password")
+                    .secret("{noop}secret")
+                    .scopes("message_read")
+                    .accessTokenValiditySeconds(600_000_000)
+                    .and()
+                .withClient("writer")
+                    .authorizedGrantTypes("password")
+                    .secret("{noop}secret")
+                    .scopes("message_write")
+                    .accessTokenValiditySeconds(600_000_000)
+                    .and()
+                .withClient("script")
+                    .authorizedGrantTypes("client_credentials")
+                    .secret("{noop}secret")
+                    .scopes("message_read")
+                    .accessTokenValiditySeconds(600_000_000)
+                    .and()
+                .withClient("web")
+                    .authorizedGrantTypes("authorization_code")
+                    .secret("{noop}secret")
+                    .scopes("message_read", "message_write")
+                    .redirectUris("http://localhost:8080/login")
+                    .accessTokenValiditySeconds(1000)
+                    .refreshTokenValiditySeconds(600_600_000)
+                    .and()
+                .withClient("mobile")
+                    .authorizedGrantTypes("implicit")
+                    .secret("{noop}secret")
+                    .scopes("message_read", "message_write")
+                    .redirectUris("http://localhost:8080/login")
+                    .accessTokenValiditySeconds(1000)
+                    .and()
+                .withClient("refresh")
+                    .authorizedGrantTypes("refresh_token")
+                    .secret("{noop}secret")
+                    .scopes("message_read", "message_write")
+                    .redirectUris("http://localhost:8080/login")
+                    .accessTokenValiditySeconds(1000)
+                    .refreshTokenValiditySeconds(100_000)
+                    .and()
+                .withClient("noscopes")
+                    .authorizedGrantTypes("password")
+                    .secret("{noop}secret")
+                    .scopes("none")
+                    .accessTokenValiditySeconds(600_000_000);
+        // @formatter:on
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        // @formatter:off
+        endpoints
+                .authenticationManager(this.authenticationManager)
+                .tokenStore(tokenStore());
+
+        if (this.jwtEnabled) {
+            endpoints.accessTokenConverter(accessTokenConverter());
+        }
+        // @formatter:on
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        if (this.jwtEnabled) {
+            return new JwtTokenStore(accessTokenConverter());
+        } else {
+            return new InMemoryTokenStore();
+        }
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setKeyPair(this.keyPair);
+
+        DefaultAccessTokenConverter accessTokenConverter = new DefaultAccessTokenConverter();
+        accessTokenConverter.setUserTokenConverter(new SubjectAttributeUserTokenConverter());
+        converter.setAccessTokenConverter(accessTokenConverter);
+
+        return converter;
+    }
+}
