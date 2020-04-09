cbe823baeb1d6a4ad8770e3454fc891c1c2a6b72
catalin-burcea <burcea.iulian.catalin@gmail.com>
2020-04-09 18:28:09 +0300

    added Spring Boot i18n examples

---
+++ spring/spring-boot-internationalization/src/main/java/ro/cburcea/playground/spring/internationalization/PageController.java
@@ -0,0 +1,13 @@
+package ro.cburcea.playground.spring.internationalization;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+ 
+    @GetMapping("/international")
+    public String getInternationalPage() {
+        return "international";
+    }
+}
