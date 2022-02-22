8142250468d8c34fc1f8f83c4c37e39b8e2a3b6b
catalin-burcea <burcea.iulian.catalin@gmail.com>
2022-02-22 12:53:39 +0200

    [spring][data] - Indexed and Named Query Parameters with @Query

--- spring/spring-data/src/main/java/ro/cburcea/playground/spring/data/AppStartupRunner.java
+++ spring/spring-data/src/main/java/ro/cburcea/playground/spring/data/AppStartupRunner.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
 @Component
 public class AppStartupRunner {
@@ -20,15 +21,23 @@ public class AppStartupRunner {
     @PostConstruct
     void init() {
         customerRepository.save(new Customer("John", "Doe", 22));
-        customerRepository.save(new Customer("John", "Smith", 22));
-        customerRepository.save(new Customer("John", "Williams", 22));
-        customerRepository.save(new Customer("John", "Cena", 22));
-        customerRepository.save(new Customer("John", "Isner", 22));
-        customerRepository.save(new Customer("John", "Walker", 22));
+        customerRepository.save(new Customer("John", "Smith", 23));
+        customerRepository.save(new Customer("John", "Williams", 24));
+        customerRepository.save(new Customer("John", "Cena", 25));
+        customerRepository.save(new Customer("John", "Isner", 26));
+        customerRepository.save(new Customer("John", "Walker", 27));
 
 
         customerRepository.findAll(PageRequest.of(1, 2)).forEach(customer -> LOG.info(customer.toString()));
         customerRepository.findAllCustomersWithPaginationUsingJpql(PageRequest.of(1, 3)).forEach(customer -> LOG.info(customer.toString()));
         customerRepository.findAllCustomersWithPaginationUsingNativeQuery(PageRequest.of(0, 2)).forEach(customer -> LOG.info(customer.toString()));
+
+        Customer customer1 = customerRepository.findCustomerByFirstNameAndLastName("John", "Cena");
+        LOG.info(customer1.toString());
+        Customer customer2 = customerRepository.findCustomerByFirstNameAndAge("John", 23);
+        LOG.info(customer2.toString());
+        customerRepository.findUserByNameList(Arrays.asList("Smith", "Cena", "Walker")).forEach(customer -> LOG.info(customer.toString()));
+
+
     }
 }
--- spring/spring-data/src/main/java/ro/cburcea/playground/spring/data/CustomerRepository.java
+++ spring/spring-data/src/main/java/ro/cburcea/playground/spring/data/CustomerRepository.java
@@ -5,19 +5,30 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    @Query(value = "SELECT u from Customer u")
+    @Query(value = "SELECT c from Customer c")
     List<Customer> findAllCustomersWithPaginationUsingJpql(Pageable pageable);
 
     @Query(value = "SELECT * FROM customers ORDER BY id desc",
             countQuery = "SELECT count(*) FROM customers",
             nativeQuery = true)
     Page<Customer> findAllCustomersWithPaginationUsingNativeQuery(Pageable pageable);
+
+    @Query("SELECT c FROM Customer c WHERE c.firstName = ?1 and c.lastName = ?2")
+    Customer findCustomerByFirstNameAndLastName(String firstName, String lastName);
+
+    @Query("SELECT c FROM Customer c WHERE c.firstName = :firstName and c.age = :age")
+    Customer findCustomerByFirstNameAndAge(@Param("firstName") String firstName, @Param("age") Integer age);
+
+    @Query(value = "SELECT c FROM Customer c WHERE c.lastName IN :names")
+    List<Customer> findUserByNameList(@Param("names") Collection<String> names);
 
 }
