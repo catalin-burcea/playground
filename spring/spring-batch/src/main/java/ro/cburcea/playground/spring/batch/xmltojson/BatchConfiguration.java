f1580c2919b88125039c9bbc17c5d3bfab7e66c0
catalin-burcea <burcea.iulian.catalin@gmail.com>
2022-03-11 15:41:29 +0200

    [spring][batch] - XML to JSON batch processing

---
+++ spring/spring-batch/src/main/java/ro/cburcea/playground/spring/batch/xmltojson/BatchConfiguration.java
@@ -0,0 +1,82 @@
+package ro.cburcea.playground.spring.batch.xmltojson;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.json.JacksonJsonObjectMarshaller;
+import org.springframework.batch.item.json.JsonFileItemWriter;
+import org.springframework.batch.item.json.builder.JsonFileItemWriterBuilder;
+import org.springframework.batch.item.xml.StaxEventItemReader;
+import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+
+    @Value("classpath:trades-input.xml")
+    private Resource inputXml;
+
+    @Value("file:trades-output.json")
+    private Resource outputXml;
+
+    @Bean
+    public StaxEventItemReader itemReader() {
+        return new StaxEventItemReaderBuilder<Trade>()
+                .name("itemReader")
+                .resource(inputXml)
+                .addFragmentRootElements("trade")
+                .unmarshaller(getJaxb2Marshaller())
+                .build();
+
+    }
+
+    @Bean
+    public JsonFileItemWriter<Trade> jsonFileItemWriter() {
+        return new JsonFileItemWriterBuilder<Trade>()
+                .jsonObjectMarshaller(new JacksonJsonObjectMarshaller<>())
+                .resource(outputXml)
+                .name("tradeJsonFileItemWriter")
+                .build();
+    }
+
+    @Bean
+    protected Step step1(ItemReader<Trade> reader, ItemWriter<Trade> writer) {
+        return stepBuilderFactory
+                .get("step1").<Trade, Trade>chunk(5)
+                .reader(reader)
+                .writer(writer)
+                .build();
+    }
+
+    @Bean(name = "firstBatchJob")
+    public Job job(@Qualifier("step1") Step step1) {
+        return jobBuilderFactory
+                .get("firstBatchJob")
+                .start(step1)
+                .build();
+    }
+
+    private Jaxb2Marshaller getJaxb2Marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setClassesToBeBound(Trade.class);
+        return marshaller;
+    }
+
+}
---
+++ spring/spring-batch/src/main/java/ro/cburcea/playground/spring/batch/xmltojson/SpringBatchXmlToJsonApp.java
@@ -0,0 +1,12 @@
+package ro.cburcea.playground.spring.batch.xmltojson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchXmlToJsonApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchXmlToJsonApp.class, args);
+    }
+}
